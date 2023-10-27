8c5a593fcddae5ba154676d8424f41c17970d1b0
Suraj Gojanur <surajgojanur@gmail.com>
2023-10-27 21:45:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,23 @@
-!pip install labelme tensorflow tensorflow-gpu opencv-python matplotlib albumentations
+#pip install labelme tensorflow tensorflow-gpu opencv-python matplotlib albumentations
 
 import os
 import time
 import uuid
 import cv2
+
+IMAGES_PATH = os.path.join('data','images')
+number_images = 30
+
+cap = cv2.VideoCapture(1)
+for imgnum in range(number_images):
+    print('Collecting image {}'.format(imgnum))
+    ret, frame = cap.read()
+    imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
+    cv2.imwrite(imgname, frame)
+    cv2.imshow('frame', frame)
+    time.sleep(0.5)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
