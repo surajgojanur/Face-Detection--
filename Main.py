8ec9c503cd8b03ee1fef37cb216bd2fc058b3694
Suraj Gojanur <surajgojanur@gmail.com>
2023-10-31 11:39:49 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,23 +1,25 @@
-#pip install labelme tensorflow tensorflow-gpu opencv-python matplotlib albumentations
-
 import os
 import time
 import uuid
 import cv2
+import numpy as np
+from google.colab.patches import cv2_imshow
 
-IMAGES_PATH = os.path.join('data','images')
+IMAGES_PATH = os.path.join('data', 'images')
 number_images = 30
 
-cap = cv2.VideoCapture(1)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
 for imgnum in range(number_images):
     print('Collecting image {}'.format(imgnum))
-    ret, frame = cap.read()
-    imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
-    cv2.imwrite(imgname, frame)
-    cv2.imshow('frame', frame)
     time.sleep(0.5)
+    uploaded_image = list(uploaded.values())[0]
+    image_array = np.frombuffer(uploaded_image, np.uint8)
+    frame = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    imgname = os.path.join(IMAGES_PATH, f'{str(uuid.uuid1())}.jpg')
+    cv2.imwrite(imgname, frame)
+    cv2_imshow(frame)
+
 cap.release()
 cv2.destroyAllWindows()
