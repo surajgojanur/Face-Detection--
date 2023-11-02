226a914c1cf1ec14a54687ef987a751931eac7c2
Suraj Gojanur <surajgojanur@gmail.com>
2023-11-02 23:34:47 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,25 +1,31 @@
+pip install --upgrade pip
+!pip install labelme tensorflow tensorflow-gpu opencv-python matplotlib albumentations
 import os
 import time
 import uuid
 import cv2
-import numpy as np
-from google.colab.patches import cv2_imshow
-
-IMAGES_PATH = os.path.join('data', 'images')
-number_images = 30
-
-os.makedirs(IMAGES_PATH, exist_ok=True)
-
+cap = cv2.VideoCapture(1)
 for imgnum in range(number_images):
     print('Collecting image {}'.format(imgnum))
-    time.sleep(0.5)
-    uploaded_image = list(uploaded.values())[0]
-    image_array = np.frombuffer(uploaded_image, np.uint8)
-    frame = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-    imgname = os.path.join(IMAGES_PATH, f'{str(uuid.uuid1())}.jpg')
+    ret, frame = cap.read()
+    imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
     cv2.imwrite(imgname, frame)
-    cv2_imshow(frame)
+    cv2.imshow('frame', frame)
+    time.sleep(0.5)
 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 cap.release()
 cv2.destroyAllWindows()
+
+# To anatate
+!labelme 
+
+import tensorflow as tf
+import json
+import numpy as np
+from matplotlib import pyplot as plt
+# Avoid OOM errors by setting GPU Memory Consumption Growth
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus: 
+    tf.config.experimental.set_memory_growth(gpu, True)
