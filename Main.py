a0c49334fd34dfa509802963e30ae42996ca9060
Suraj Gojanur <surajgojanur@gmail.com>
2023-11-03 19:38:35 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -29,3 +29,13 @@ from matplotlib import pyplot as plt
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus: 
     tf.config.experimental.set_memory_growth(gpu, True)
+
+tf.config.list_physical_devices('GPU')
+images = tf.data.Dataset.list_files('data\\images\\*.jpg')
+
+images.as_numpy_iterator().next()
+
+def load_image(x): 
+    byte_img = tf.io.read_file(x)
+    img = tf.io.decode_jpeg(byte_img)
+    return img
